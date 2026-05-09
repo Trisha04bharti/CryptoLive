75b0cef529bd61f0221e37ab1bb5f17596a93f74
Trisha Bharti <trishabharti444@gmail.com>
2026-05-09 14:05:55 +0530

    Initial Commit

---
+++ CryptoLive/CryptoLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLiveApp.swift
+//  CryptoLive
+//
+//  Created by Vikram Kumar on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
